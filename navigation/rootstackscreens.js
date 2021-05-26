3829959eca55b8af31dd5e4829599747e92f39ca
Neelesh Ranjan Jha <neeleshranjan.jha2019@vitstudent.ac.in>
2021-05-26 05:54:09 +0530

    Loading screen added

--- navigation/rootstackscreens.js
+++ navigation/rootstackscreens.js
@@ -4,33 +4,20 @@ import SignIn from '../screens/signin';
 import SignUp from '../screens/signup';
 import Meds from '../screens/meds';
 import AddMeds from '../screens/add';
-import { AsyncStorage } from 'react-native';
+import Loading from '../screens/loading';
+
 const RootStack = createStackNavigator();
 
 
 const RootStackScreen = () => {
-    async function getToken(){
-        return AsyncStorage.getItem('authToken');
-    }
-
+    
     return(
         <RootStack.Navigator headerMode="none">
-        {getToken() == '' ? (
-        <>
+            <RootStack.Screen name='Loading' component={Loading} />
             <RootStack.Screen name='SignIn' component={SignIn} />
             <RootStack.Screen name='SignUp' component={SignUp} />
             <RootStack.Screen name='Meds' component={Meds} />
             <RootStack.Screen name='AddMeds' component={AddMeds} />
-        </>
-        ) : (
-        <>
-            <RootStack.Screen name='Meds' component={Meds} />
-            <RootStack.Screen name='AddMeds' component={AddMeds} />
-            <RootStack.Screen name='SignIn' component={SignIn} />
-            <RootStack.Screen name='SignUp' component={SignUp} />
-        </>
-        )
-        }
         </RootStack.Navigator>
     );  
 };
@@ -38,4 +25,3 @@ const RootStackScreen = () => {
 
 
 export default RootStackScreen;
-    
